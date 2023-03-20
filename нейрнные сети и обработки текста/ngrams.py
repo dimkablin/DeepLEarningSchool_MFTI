caa8ee14827e777c76c3a11d7e06c573b39a48e1
zanoo <dmitryellison@gmail.com>
2023-03-20 18:38:00 +0300

    added нейронные сети и обработка текста adeed ngram.py

---
+++ нейрнные сети и обработки текста/ngrams.py
@@ -0,0 +1,14 @@
+
+def ngram(words=["Hello", "world"], n=[2, 3]):
+    result = []
+
+    for word in words:
+        for length in n:
+            for i in range(len(word)-length+1):
+                result.append(word[i:i+length])
+
+    return result
+
+
+word = "язык"
+print(ngram([word]))
